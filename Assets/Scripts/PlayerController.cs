527c07424400c65c4251f60b8175cf946bc42780
Julianacavalaro <juliana.cavalaro@hotmail.com>
2025-12-12 11:53:28 -0300

    Arruma bug movimento em somente uma direção

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,30 @@
+using UnityEditor;
 using UnityEngine;
 
 public class PlayersController : MonoBehaviour
 {
+    public float speed;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    private void FixedUpdate()
+    {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(x, 0, y);
+        force.Normalize();
+        force *= speed;
+        rb.AddForce(force);
     }
 }
