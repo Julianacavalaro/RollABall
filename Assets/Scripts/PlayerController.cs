dae511df00b813cbfe5cbd09744eb466ff04e935
Julianacavalaro <juliana.cavalaro@hotmail.com>
2025-12-12 11:01:20 -0300

    Add eixo X e Y no controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayersController : MonoBehaviour
@@ -13,6 +11,7 @@ public class PlayersController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
     }
 }
